a4f7077d1eae401c11698c54841628595aa2f8ec
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-18 16:30:09 +0530

    intead of handler gave props name clicked and changed

--- src/Components/Persons/Persons.jsx
+++ src/Components/Persons/Persons.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import Person from "./Person/Person";
+
 const Persons = (props) => {
   return (
     <div>
-      {" "}
       {props.persons.map((person, index) => {
         return (
           <Person
-            click={() => props.deletePersonHandler(index)}
+            click={() => props.cliked(index)}
             name={person.name}
             age={person.age}
             key={person.id}
-            changed={(event) => props.nameChangedHandler(event, person.id)}
+            changed={(event) => props.changed(event, person.id)}
           />
         );
       })}
