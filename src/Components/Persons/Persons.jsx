211c220b6ce5e5446c4f5ef5a35d0fac5a82cedc
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-18 16:26:33 +0530

    imported person comp into persons comp

--- src/Components/Persons/Persons.jsx
+++ src/Components/Persons/Persons.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import person from "./Person/Person";
 const Persons = (props) => {
-  return <div></div>;
+  return (
+    <div>
+      {" "}
+      {props.persons.map((person, index) => {
+        return (
+          <Person
+            click={() => this.deletePersonHandler(index)}
+            name={person.name}
+            age={person.age}
+            key={person.id}
+            changed={(event) => this.nameChangedHandler(event, person.id)}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default Persons;
