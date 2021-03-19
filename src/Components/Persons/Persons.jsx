3f627ce3c9cedc2a1371ca2b993df5bbd2d4ffaa
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-19 22:17:41 +0530

    successfully converted persons into class based compoent

--- src/Components/Persons/Persons.jsx
+++ src/Components/Persons/Persons.jsx
@@ -6,14 +6,14 @@ class Persons extends Component {
     console.log("[Person.js] rendering...");
     return (
       <div>
-        {props.persons.map((person, index) => {
+        {this.props.persons.map((person, index) => {
           return (
             <Person
-              click={() => props.clicked(index)}
+              click={() => this.props.clicked(index)}
               name={person.name}
               age={person.age}
               key={person.id}
-              changed={(event) => props.changed(event, person.id)}
+              changed={(event) => this.props.changed(event, person.id)}
             />
           );
         })}
