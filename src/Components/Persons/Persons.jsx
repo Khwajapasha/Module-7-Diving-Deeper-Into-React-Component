d1f953164aa35c2798a785fb0e3e5370b5955103
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-19 22:13:12 +0530

    converting persons into class component

--- src/Components/Persons/Persons.jsx
+++ src/Components/Persons/Persons.jsx
@@ -1,26 +1,25 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import Person from "./Person/Person";
 
 class Persons extends Component {
-  render (){
-    
+  render() {
+    console.log("[Person.js] rendering...");
+    return (
+      <div>
+        {props.persons.map((person, index) => {
+          return (
+            <Person
+              click={() => props.clicked(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => props.changed(event, person.id)}
+            />
+          );
+        })}
+      </div>
+    );
   }
-  console.log("[Person.js] rendering...");
-  return (
-    <div>
-      {props.persons.map((person, index) => {
-        return (
-          <Person
-            click={() => props.clicked(index)}
-            name={person.name}
-            age={person.age}
-            key={person.id}
-            changed={(event) => props.changed(event, person.id)}
-          />
-        );
-      })}
-    </div>
-  );
-};
+}
 
 export default Persons;
